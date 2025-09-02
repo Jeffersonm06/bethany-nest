2c6667a70196931dace2181eccf914ea029ca830
Jeffersonm06 <wellingtonalexandre857@gmail.com>
2025-09-02 18:27:52 -0300

    feat: ajustes no chat

--- src/bethany/bethany.controller.ts
+++ src/bethany/bethany.controller.ts
@@ -3,26 +3,24 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { Bethany } from './bethany.service';
 import { UserId } from 'src/decorator /user-id.decorator';
 
-interface ChatDto {
-  userId?: string; // opcional se quiser identificar o usuário
+export class ChatDto {
   message: string;
 }
 
 @Controller('bethany')
 export class BethanyController {
-  constructor(private readonly bethanyService: Bethany) {}
+  constructor(private readonly bethanyService: Bethany) { }
 
   @Post('chat')
-  async sendMessage(@Body() body: ChatDto, @UserId() userId:string) {
-    console.log(body)
-    const { message } = body;
+  async sendMessage(@Body('message') message: string, @UserId() userId: string) {
+    console.log('Mensagem recebida do front:', message);
 
     if (!message || !message.trim()) {
       return { error: 'Mensagem vazia' };
     }
 
     const responseText = await this.bethanyService.sendMessage(message, userId);
-
     return { message: responseText };
   }
+
 }
--- src/bethany/bethany.service.ts
+++ src/bethany/bethany.service.ts
@@ -17,25 +17,25 @@ export class Bethany {
     private readonly model = 'gemini-2.5-flash';
     private readonly messagesPath = 'messages';
     private readonly instructions = `
-Você é Bethany, uma assistente prática, estilosa e acolhedora, com um toque feminino e inspirador. 
-Sua missão é ajudar criadoras de conteúdo a se sentirem confiantes e à vontade para criar, oferecendo ideias criativas e rápidas para redes sociais como TikTok, Instagram, Facebook e YouTube.
-Fluxo da conversa:
-1. Cumprimente a usuária de forma calorosa.
-2. caso o usuário queira falar sobre criação de conteudo
---1. Pergunte qual tipo de conteúdo ela quer criar.
---2. Pergunte para qual rede social será.
---3. Com base nas respostas, traga ideias criativas e dicas objetivas, seguindo o padrão:
---4 ideias principais (claras e curtas).
---5 ou 2 dicas rápidas para melhorar o conteúdo.
-Linguagem simples, amigável e motivadora.
-Extra:
-Sempre que possível, inclua dicas atuais e relevantes sobre moda, maquiagem e tendências virais que possam inspirar o conteúdo.
-Essas dicas devem ser curtas, práticas e adaptadas ao perfil da criadora.
-Regras:
-Vá direto ao ponto, sem enrolar.
-Evite blocos de texto grandes.
-Sempre termine incentivando a criadora a colocar as ideias em prática.
-`
+        Você é Bethany, uma assistente prática, estilosa e acolhedora, com um toque feminino e inspirador. 
+        Sua missão é ajudar criadoras de conteúdo a se sentirem confiantes e à vontade para criar, oferecendo ideias criativas e rápidas para redes sociais como TikTok, Instagram, Facebook e YouTube.
+        Fluxo da conversa:
+        1. Cumprimente a usuária de forma calorosa.
+        2. caso o usuário queira falar sobre criação de conteudo
+        --1. Pergunte qual tipo de conteúdo ela quer criar.
+        --2. Pergunte para qual rede social será.
+        --3. Com base nas respostas, traga ideias criativas e dicas objetivas, seguindo o padrão:
+        --4 ideias principais (claras e curtas).
+        --5 ou 2 dicas rápidas para melhorar o conteúdo.
+        Linguagem simples, amigável e motivadora.
+        Extra:
+        Sempre que possível, inclua dicas atuais e relevantes sobre moda, maquiagem e tendências virais que possam inspirar o conteúdo.
+        Essas dicas devem ser curtas, práticas e adaptadas ao perfil da criadora.
+        Regras:
+        Vá direto ao ponto, sem enrolar.
+        Evite blocos de texto grandes.
+        Sempre termine incentivando a criadora a colocar as ideias em prática.
+        `
 
     constructor(
         private readonly firebase: FirebaseService,
@@ -50,22 +50,18 @@ Sempre termine incentivando a criadora a colocar as ideias em prática.
             );
         }
 
-        // Pegar últimas 15 mensagens para contexto
         const history = await this.getUserMessages(userId, 15);
 
-        // Montar contexto pro modelo
         const contents = history.map((msg) => ({
             role: msg.sender === 'user' ? 'user' : 'model',
             parts: [{ text: msg.message }],
         }));
 
-        // Adiciona a nova mensagem do usuário ao contexto
         contents.push({
             role: 'user',
             parts: [{ text }],
         });
 
-        // Chama o modelo
         const response = await this.ai.models.generateContent({
             model: this.model,
             contents,
@@ -79,7 +75,6 @@ Sempre termine incentivando a criadora a colocar as ideias em prática.
 
         const reply = response.text || '...';
 
-        // Salvar mensagem do usuário
         await this.saveMessage({
             sender: 'user',
             userId,
@@ -87,7 +82,6 @@ Sempre termine incentivando a criadora a colocar as ideias em prática.
             timestamp: Date.now(),
         });
 
-        // Salvar resposta da Bethany
         await this.saveMessage({
             sender: 'bethany',
             userId,
