f65ae637b02b384f0ff29531728a7289ce829bfb
Jeffersonm06 <wellingtonalexandre857@gmail.com>
2025-09-07 12:10:41 -0300

    ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './auth/auth.module';
 import { BethanyModule } from './bethany/bethany.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+     ConfigModule.forRoot({
+      isGlobal: true, // deixa o ConfigModule disponível em todo o app
+    }),
     UserModule,
     FirebaseModule,
     AuthModule,
--- src/bethany/bethany.service.ts
+++ src/bethany/bethany.service.ts
@@ -1,10 +1,11 @@
 import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
-import { GoogleGenAI, Part } from '@google/genai';
+import { GoogleGenAI, Part, Modality } from '@google/genai';
 import { FirebaseService } from 'src/firebase/firebase.service';
 import { UserService } from 'src/user/user.service';
 import * as fs from 'fs';
 import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
+import { ConfigService } from '@nestjs/config';
 
 
 export interface Message {
@@ -20,7 +21,7 @@ export interface Message {
 export class Bethany {
     private uploadDir = path.join(__dirname, '..', '..', 'uploads', 'chat-images');
 
-    private ai = new GoogleGenAI({});
+    private ai: GoogleGenAI;
     private readonly model = 'gemini-2.5-flash';
     private readonly messagesPath = 'messages';
     private readonly instructions = `
@@ -43,193 +44,208 @@ export class Bethany {
         Evite blocos de texto grandes.
         Sempre termine incentivando a criadora a colocar as ideias em prática.
         `
-
+    private readonly image_instructions = `
+    
+    `
     constructor(
         private readonly firebase: FirebaseService,
         private readonly us: UserService,
+        private configService: ConfigService,
     ) {
         if (!fs.existsSync(this.uploadDir)) {
             fs.mkdirSync(this.uploadDir, { recursive: true });
         }
+        const apiKey = this.configService.get<string>('GEMINI_API_KEY');
+        if (!apiKey) {
+            throw new Error('GEMINI_API_KEY não definido no ambiente');
+        }
+        this.ai = new GoogleGenAI({ apiKey: apiKey });
     }
 
-    async sendMessage(text: string, userId: string, image?: Express.Multer.File, typeMsg: Message['type'] = 'text'): Promise<Message> {
-        const user = await this.us.findOne(userId);
-        if (!user) throw new NotFoundException('O usuário deve estar logado para acessar o chat');
+    async sendMessage(text: string, userId: string, image ?: Express.Multer.File, typeMsg: Message['type'] = 'text'): Promise < Message > {
+    const user = await this.us.findOne(userId);
+    if(!user) throw new NotFoundException('O usuário deve estar logado para acessar o chat');
 
-        const history = await this.getUserMessages(userId, 15);
+    const history = await this.getUserMessages(userId, 15);
 
-        const formattedHistory = history
-            .map(m => `${m.sender === 'user' ? 'Usuário' : 'Bethany'}: ${m.content}`)
-            .join('\n');
+    const formattedHistory = history
+        .map(m => `${m.sender === 'user' ? 'Usuário' : 'Bethany'}: ${m.content}`)
+        .join('\n');
 
-        const messageToSend = `
+    const messageToSend = `
 Contexto da conversa:
         ${formattedHistory}
 
 Mensagem atual do usuário (${user.username}):${text}
         `;
 
-        let contents: any;
-        let base64ImageData = "";
-        let savedFileUrl: string | undefined;
+    let contents: any;
+    let base64ImageData = "";
+    let savedFileUrl: string | undefined;
 
-        const timestamp = Date.now();
-        const userMessage: Message = {
-            sender: 'user',
-            userId,
-            content: text,
-            timestamp,
-            type: typeMsg
-        };
-        let bethanyMessage: Message;
-        if (image) {
-            savedFileUrl = await this.saveUserImage(image);
+    const timestamp = Date.now();
+    const userMessage: Message = {
+        sender: 'user',
+        userId,
+        content: text,
+        timestamp,
+        type: typeMsg
+    };
+    let bethanyMessage: Message;
+    if(image) {
+        savedFileUrl = await this.saveUserImage(image);
 
-            userMessage.type = 'image'
-            userMessage.image = savedFileUrl;
-            base64ImageData = image.buffer.toString('base64');
-            contents = [{ text: messageToSend }, {
-                inlineData: {
-                    mimeType: image.mimetype,
-                    data: base64ImageData
-                }
-            }]
-        } else {
-            contents = [{ text: messageToSend }]
-        }
+        userMessage.type = 'image'
+        userMessage.image = savedFileUrl;
+        base64ImageData = image.buffer.toString('base64');
+        contents = [{ text: messageToSend }, {
+            inlineData: {
+                mimeType: image.mimetype,
+                data: base64ImageData
+            }
+        }]
+    } else {
+        contents =[{ text: messageToSend }]
+    }
 
         await this.saveMessage(userMessage);
 
-        if (typeMsg == 'search') {
-            // Faz a pesquisa e retorna HTML formatado
-            const searchHtml = await this.search(text);
-            bethanyMessage = {
-                sender: 'bethany',
-                userId,
-                content: searchHtml,
-                timestamp: Date.now(),
-                type: 'search'
-            };
+    if(typeMsg == 'search') {
+    // Faz a pesquisa e retorna HTML formatado
+    const searchHtml = await this.search(text);
+    bethanyMessage = {
+        sender: 'bethany',
+        userId,
+        content: searchHtml,
+        timestamp: Date.now(),
+        type: 'search'
+    };
 
-            await this.saveMessage(bethanyMessage);
+    await this.saveMessage(bethanyMessage);
 
-            return bethanyMessage;
-        }
+    return bethanyMessage;
+}
 
-        const response = await this.ai.models.generateContent({
-            model: this.model,
-            contents,
-            config: {
-                tools: [
-                    { urlContext: {} },
-                ],
-                thinkingConfig: { thinkingBudget: 0 },
-                systemInstruction: this.instructions
-            }
-        });
+const response = await this.ai.models.generateContent({
+    model: this.model,
+    contents,
+    config: {
+        tools: [
+            { urlContext: {} },
+        ],
+        thinkingConfig: { thinkingBudget: 0 },
+        systemInstruction: this.instructions
+    }
+});
 
-        let formatedResponse = response.text || "..."
+let formatedResponse = response.text || "..."
 
-        bethanyMessage = { sender: 'bethany', userId, content: formatedResponse, timestamp: Date.now(), type: "text" };
-        await this.saveMessage(bethanyMessage);
+bethanyMessage = { sender: 'bethany', userId, content: formatedResponse, timestamp: Date.now(), type: "text" };
+await this.saveMessage(bethanyMessage);
 
-        return bethanyMessage;
+return bethanyMessage;
     }
 
-    async saveUserImage(file: Express.Multer.File): Promise<string> {
-        if (!file) {
-            throw new BadRequestException("Nenhum arquivo enviado");
-        }
+    async saveUserImage(file: Express.Multer.File): Promise < string > {
+    if(!file) {
+        throw new BadRequestException("Nenhum arquivo enviado");
+    }
 
         // Extensões permitidas
         const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif", ".webp"];
-        const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
 
-        const extension = path.extname(file.originalname).toLowerCase();
-        const mimeType = file.mimetype;
+    const extension = path.extname(file.originalname).toLowerCase();
+    const mimeType = file.mimetype;
 
-        if (!allowedExtensions.includes(extension) || !allowedMimeTypes.includes(mimeType)) {
-            throw new BadRequestException("Formato de imagem inválido");
-        }
+    if(!allowedExtensions.includes(extension) || !allowedMimeTypes.includes(mimeType)) {
+    throw new BadRequestException("Formato de imagem inválido");
+}
 
-        // Nome único com UUID
-        const filename = `${uuidv4()}${extension}`;
-        const filepath = path.join(this.uploadDir, filename);
+// Nome único com UUID
+const filename = `${uuidv4()}${extension}`;
+const filepath = path.join(this.uploadDir, filename);
 
-        await fs.promises.writeFile(filepath, file.buffer);
+await fs.promises.writeFile(filepath, file.buffer);
 
-        return `/uploads/chat-images/${filename}`;
+return `/uploads/chat-images/${filename}`;
     }
 
 
-    async search(query: string): Promise<string> {
-        try {
-            const groundingTool = { googleSearch: {} };
-            const config = { tools: [groundingTool] };
+    async search(query: string): Promise < string > {
+    try {
+        const groundingTool = { googleSearch: {} };
+        const config = { tools: [groundingTool] };
 
-            const response = await this.ai.models.generateContent({
-                model: 'gemini-2.5-flash',
-                contents: [{ role: 'user', parts: [{ text: query }] }],
-                config,
-            });
+        const response = await this.ai.models.generateContent({
+            model: 'gemini-2.5-flash',
+            contents: [{ role: 'user', parts: [{ text: query }] }],
+            config,
+        });
 
-            if (!response?.candidates?.length) return `<p>Nenhum resultado encontrado.</p>`;
+        if(!response?.candidates?.length) return `<p>Nenhum resultado encontrado.</p>`;
 
-            let html = '';
+        let html = '';
 
-            for (const candidate of response.candidates) {
-                const content = candidate.content;
-                if (!content) continue;
+        for(const candidate of response.candidates) {
+    const content = candidate.content;
+    if (!content) continue;
 
-                // texto principal
-                content.parts?.forEach(part => {
-                    html += `${part.text}`
-                });
+    // texto principal
+    content.parts?.forEach(part => {
+        html += `${part.text}`
+    });
 
-                // links das fontes
-                const groundingChunks = candidate.groundingMetadata?.groundingChunks;
-                if (groundingChunks?.length) {
-                    html += `<ul>`;
-                    groundingChunks.forEach(chunk => {
-                        const uri = chunk.web?.uri;
-                        const title = chunk.web?.title || uri;
-                        if (uri) html += `<li><a href="${uri}" target="_blank">${title}</a></li>`;
-                    });
-                    html += `</ul>`;
-                }
-            }
+    // links das fontes
+    const groundingChunks = candidate.groundingMetadata?.groundingChunks;
+    if (groundingChunks?.length) {
+        html += `<ul>`;
+        groundingChunks.forEach(chunk => {
+            const uri = chunk.web?.uri;
+            const title = chunk.web?.title || uri;
+            if (uri) html += `<li><a href="${uri}" target="_blank">${title}</a></li>`;
+        });
+        html += `</ul>`;
+    }
+}
 
-            return html;
+return html;
 
         } catch (error) {
-            console.error(`Erro na busca: ${error.message}`, error.stack);
-            return `<p>Ocorreu um erro na busca.</p>`;
-        }
+    console.error(`Erro na busca: ${error.message}`, error.stack);
+    return `<p>Ocorreu um erro na busca.</p>`;
+}
     }
+
+    async genImage(message: Message) {
+    const response = await this.ai.models.generateContent({
+        model: "gemini-2.5-flash-image-preview",
+        contents: message.content,
+    });
+}
 
     async getUserMessages(
-        userId: string,
-        limit: number,
-    ): Promise<Message[]> {
-        const allMessages = await this.firebase.findAll<Message>(
-            `${this.messagesPath}/${userId}`,
-        );
+    userId: string,
+    limit: number,
+): Promise < Message[] > {
+    const allMessages = await this.firebase.findAll<Message>(
+        `${this.messagesPath}/${userId}`,
+    );
 
-        if (!allMessages) return [];
+    if(!allMessages) return [];
 
-        const msgs = Object.values(allMessages).sort(
-            (a, b) => a.timestamp - b.timestamp,
-        );
+    const msgs = Object.values(allMessages).sort(
+        (a, b) => a.timestamp - b.timestamp,
+    );
 
-        return msgs.slice(-limit);
-    }
+    return msgs.slice(-limit);
+}
 
-    private async saveMessage(message: Message): Promise<void> {
-        await this.firebase.create<Message>(
-            `${this.messagesPath}/${message.userId}`,
-            message,
-        );
-    }
+    private async saveMessage(message: Message): Promise < void> {
+    await this.firebase.create<Message>(
+        `${this.messagesPath}/${message.userId}`,
+        message,
+    );
+}
 
 }
